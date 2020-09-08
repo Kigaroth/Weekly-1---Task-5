f34ff20eff487ce1423b0431718fad2f0eb5b2d4
Kigaroth <hp_20@hotmail.com>
2020-09-08 15:04:24 +0200

    Fixed! Text color value for white was initially written as 0, but 0 gives black. Changed to 7 now.

--- Weekly 1 - Task 5/Weekly 1 - Task 5.cpp
+++ Weekly 1 - Task 5/Weekly 1 - Task 5.cpp
@@ -2,21 +2,21 @@
 
 #include <iostream>
 #include <string>
-#include <windows.h>
+#include <windows.h> //included in order to achieve textcolor//
 
 int main()
 {
     std::string rows[4] = { "A", "B", "C", "D" };
 
     //Column header//
-    std::cout << " ";
+    std::cout << "   ";
     for (int i = 1; i <= 6; i++) {
-        std::cout << " " << i << " ";
+        std::cout << " " << i << "  ";
     }
     std::cout << "\n";
 
     //Horizontal line//
-    std::cout << " ";
+    std::cout << "   ";
     for (int j = 0; j < 6; j++) {
         std::cout << "--- ";
     }
@@ -24,22 +24,28 @@ int main()
 
     //Printing rows//
     for (int i = 0; i < sizeof(rows) / sizeof(rows[0]); i++) {
-        
+
         //Print 6 columns//
         std::cout << rows[i] << " |";
-            for (int j = 0; j < 6; j++) {
-                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
-                std::cout << " *";
-                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0);
-                std::cout << "|";
+        for (int j = 0; j < 6; j++) {
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12); //textcolor change to red for *//
+            std::cout << " * ";
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7); //textcolor changes back to white//
+            std::cout << "|";
         }
-            std::cout << " " << rows[i] << "\n";
+        std::cout << " " << rows[i] << "\n";
         //Print Horizontal line//
-            std::cout << "  ";
-            for (int j = 0; j < 6; j++) {
-                std::cout << "--- ";
-            }
-            std::cout << "\n";
+        std::cout << "  ";
+        for (int j = 0; j < 6; j++) {
+            std::cout << "--- ";
+        }
+        std::cout << "\n";
     }
+
+            std::cout << "   ";
+            for (int i = 1; i <= 6; i++) {
+                std::cout << " " << i << "  ";
+            }
+    
 }
 
