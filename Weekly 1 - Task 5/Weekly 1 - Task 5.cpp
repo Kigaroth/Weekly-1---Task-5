8f66e36bae465fccd7e245ce7793b4e456bcc237
Kigaroth <hp_20@hotmail.com>
2020-09-08 14:49:47 +0200

    Using loops to create figure

--- Weekly 1 - Task 5/Weekly 1 - Task 5.cpp
+++ Weekly 1 - Task 5/Weekly 1 - Task 5.cpp
@@ -1,9 +1,45 @@
 // Weekly 1 - Task 5.cpp 
 
 #include <iostream>
+#include <string>
+#include <windows.h>
 
 int main()
 {
-    
+    std::string rows[4] = { "A", "B", "C", "D" };
+
+    //Column header//
+    std::cout << " ";
+    for (int i = 1; i <= 6; i++) {
+        std::cout << " " << i << " ";
+    }
+    std::cout << "\n";
+
+    //Horizontal line//
+    std::cout << " ";
+    for (int j = 0; j < 6; j++) {
+        std::cout << "--- ";
+    }
+    std::cout << "\n";
+
+    //Printing rows//
+    for (int i = 0; i < sizeof(rows) / sizeof(rows[0]); i++) {
+        
+        //Print 6 columns//
+        std::cout << rows[i] << " |";
+            for (int j = 0; j < 6; j++) {
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
+                std::cout << " *";
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0);
+                std::cout << "|";
+        }
+            std::cout << " " << rows[i] << "\n";
+        //Print Horizontal line//
+            std::cout << "  ";
+            for (int j = 0; j < 6; j++) {
+                std::cout << "--- ";
+            }
+            std::cout << "\n";
+    }
 }
 
